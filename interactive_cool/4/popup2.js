c960bcda085ab66e4a590b1f6e3f92a62b9acc16
roroc22 <rororoc22@gmail.com>
2023-02-06 09:12:40 +0800

    upload 123

---
+++ interactive_cool/4/popup2.js
@@ -0,0 +1,44 @@
+//popup
+var delay2 = 6000; //in milleseconds
+
+jQuery(document).ready(function($){
+  setTimeout(function(){ showpopup2Popup(); }, delay2);
+  
+  $('.popup-close2').click(function(){
+      $('.popup2-overlay').hide();
+      
+      //when closed create a cookie to prevent popup to show again on refresh
+      setCookie('popup2-popup', 'popped', 30);
+  });
+});
+
+function showpopup2Popup(){
+  if( getCookie('popup2-popup') == ""){
+    $('.popup2-overlay').show();
+    setCookie('popup2-popup', 'popped', 30);
+  }
+  else{
+    console.log("popup2 popup blocked.");
+  }
+}
+
+
+function setCookie(cname,cvalue,exdays)
+{
+    var d = new Date();
+    d.setTime(d.getTime()+(exdays*24*60*60*1000));
+    var expires = "expires="+d.toGMTString();
+    document.cookie = cname+"="+cvalue+"; "+expires+"; path=/";
+}
+
+function getCookie(cname)
+{
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) 
+    {
+        var c = jQuery.trim(ca[i]);
+        if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
